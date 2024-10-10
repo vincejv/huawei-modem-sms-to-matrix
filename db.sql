a33aeb585dd0cb504c57cc486ebd0e540174daf3
Vince JV <1276544+vincejv@users.noreply.github.com>
2024-10-11 03:47:52 +0800

    Update DB Schema

--- db.sql
+++ db.sql
@@ -3,4 +3,5 @@ CREATE TABLE last_message (
     date TIMESTAMP
 );
 INSERT INTO last_message (id, date) VALUES (1, '1970-01-01 00:00:00');
+INSERT INTO last_message (id, date) VALUES (2, '1970-01-01 00:00:00');
 ALTER TABLE last_message ADD COLUMN index INTEGER DEFAULT -1;
